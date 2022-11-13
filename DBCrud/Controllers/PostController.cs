2ec0a8e56e5d82e3fd7408926944b224657359a4
Pili_zr36 <Pili_zr36@itstepru.onmicrosoft.com>
2022-11-13 12:24:31 +0700

    Add project files.

---
+++ DBCrud/Controllers/PostController.cs
@@ -0,0 +1,95 @@
+﻿using DBCrud.Helpers;
+using DBCrud.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DBCrud.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly BlogDbContext blogDbContext;
+
+        public PostController(BlogDbContext blogDbContext)
+        {
+            this.blogDbContext = blogDbContext;
+        }
+
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            //ModelState.AddModelError("Title", "99999");
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(blogDbContext.Posts.ToList());
+        }
+
+        [HttpPost]
+        //  [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Add(Post post, IFormFile ImageUrl)
+        {
+            try
+            {
+                post.ImageUrl = await FileUploadHelper.Upload(ImageUrl);
+            }
+            catch (Exception) { }
+
+
+            if (ModelState.IsValid)
+            {
+                post.Date = DateTime.Now;
+
+                blogDbContext.Posts.AddAsync(post);
+                await blogDbContext.SaveChangesAsync();
+                TempData["Status"] = "New post added!";
+                return RedirectToAction("Index");
+            }
+
+            return View(post);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            Post post = blogDbContext.Posts.Find(id);
+            if (post != null)
+            {
+                blogDbContext.Posts.Remove(post);
+                //db.SaveChanges();
+                await blogDbContext.SaveChangesAsync();
+            }
+            return RedirectToAction("Index");
+            //return View(post);
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            Post post = blogDbContext.Posts.Find(id);
+            return View(post);
+        }
+
+        /*[HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            Post post = blogDbContext.Posts.Find(id);
+            if (post != null)
+            {
+                post.Title = "qwert";
+                //blogDbContext.Posts.Remove(post);
+                //db.SaveChanges();
+                await blogDbContext.SaveChangesAsync();
+            }
+            return RedirectToAction("Index");
+            return View(post);*/
+        }
+
+    }
+}
---
+++ DBCrud/Migrations/20221105181557_initDb.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DBCrud.Migrations
+{
+    public partial class initDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
